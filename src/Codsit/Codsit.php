3504840af448ad061cf89b82b89300934a47ea6b
Jhordy Huaman <jhordyho@upeu.edu.pe>
2018-10-14 02:08:03 -0500

    vercion 0.1.1

--- src/Codsit/Codsit.php
+++ src/Codsit/Codsit.php
@@ -4,11 +4,12 @@ namespace Codsit;
 use Codsit\Error as Errors;
 
 class Codsit{
-    const BASE_URL = 'http://soft-fe.test' ;
+    //onst BASE_URL = 'http://soft-fe.test' ;
+    const BASE_URL = 'https://factura.codsit.com' ;
     public $api_key;
 
     const API_VERSION = 'V0.1';
-    //const BASE_URL = 'https://codsit.com/facturape/v1';
+
 
     public function __construct($options){
         $this->api_key = $options["api_key"];
